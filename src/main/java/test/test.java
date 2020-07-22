ccddec28b257d00c749d0cbac73a7da24ad72302
bx1 <bx1@bd.com>
2020-07-22 16:26:25 +0800

    最新修改

--- src/main/java/test/test.java
+++ src/main/java/test/test.java
@@ -4,6 +4,6 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("测试");
 		System.out.println("demo");
-		System.out.println("测试");
+		System.out.println("测试提交后修改");
 	}
 }
